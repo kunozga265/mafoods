985f4c0541604f80b6c9d9e9e424d1df25549cb9
Kunozga Mlowoka <kunozgamlowoka@gmail.com>
2024-09-19 07:18:57 +0200

    Returning one role

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,7 +19,7 @@ class UserResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            'roles' => $this->roles,
+            'role' => $this->roles()->first(),
             'phone_number' => $this->phone_number,
         ];
     }
